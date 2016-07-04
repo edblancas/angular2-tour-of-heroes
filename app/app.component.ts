a80a0dce93884d0eabeee3bbd0b364a5797777e9
Daniel Blancas <edblancas@gmail.com>
2016-07-04 17:46:24 -0500

    Step 4

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
-export class Hero {
-  id: number;
-  name: string;
-}
+import { HeroDetailComponent } from './hero-detail.component';
+import { Hero } from './hero';
+
 const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Celeritas'},
+  {id: 15, name: 'Magneta'},
+  {id: 16, name: 'RubberMan'},
+  {id: 17, name: 'Dynama'},
+  {id: 18, name: 'Dr IQ'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
 ];
 @Component({
   selector: 'my-app',
@@ -28,12 +27,7 @@ const HEROES: Hero[] = [
       </li>
     </ul>
     <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
+        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     </div>
   `,
   styles: [`
@@ -84,11 +78,15 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  directives: [HeroDetailComponent]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
   selectedHero: Hero;
-  onSelect(hero: Hero) { this.selectedHero = hero; }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
