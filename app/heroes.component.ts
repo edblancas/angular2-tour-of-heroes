933e8eb7f827f3012675193a3b8bafc49b3b301c
Daniel Blancas <edblancas@gmail.com>
2016-07-04 23:30:36 -0500

    Step 7. Finished

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -2,16 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 import { Router } from '@angular/router';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
   styleUrls: ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit {
   title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
+  error: any;
+  addingHero = false;
 
   constructor(private heroService: HeroService,
               private router: Router) {
@@ -26,10 +30,31 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes() {
-    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
   gotoDetail() {
     this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  addHero() {
+    this.addingHero = true;
+    this.selectedHero = null;
+  }
+
+  close(savedHero: Hero) {
+    this.addingHero = false;
+    if (savedHero) { this.getHeroes(); }
+  }
+
+  deleteHero(hero: Hero, event: any) {
+    event.stopPropagation();
+    this.heroService
+      .delete(hero)
+      .then(res => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      })
+      .catch(error => this.error = error);
   }
 }
